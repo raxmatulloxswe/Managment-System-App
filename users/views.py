e3cb30309e9e4f186d7a2321f0907c327a7520f5
Rahmatjon <rahmatjonibragimov17@gmail.com>
2024-11-01 16:38:51 +0500

    added VerifyEmail

--- users/views.py
+++ users/views.py
@@ -1,6 +1,12 @@
 from rest_framework import generics, status
 from .serializers import RegisterSerializer
 from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.contrib.sites.shortcuts import get_current_site
+from django.urls import reverse
+
+from .models import User
+from .utils import Util
 
 
 # Create your views here.
@@ -11,6 +17,24 @@ class RegisterView(generics.GenericAPIView):
         serializer = self.serializer_class(data=user)
         serializer.is_valid(raise_exception=True)
         serializer.save()
-
         user_data = serializer.data
+
+        user=User.objects.get(email=user_data['email'])
+
+        token = RefreshToken.for_user(user).access_token
+
+        current_site = get_current_site(request).domain
+        relative_link = reverse('verify-email')
+        adsurl = 'http://'+current_site+relative_link+"?token="+str(token)
+        email_body = 'Hi '+user.username+'Use link below to verify \n'+ adsurl
+        data={'email_body': email_body,
+              'to_email': user.email,
+              'email_subject': 'Verify your email'}
+        Util.send_email(data)
+
         return Response(user_data, status=status.HTTP_201_CREATED)
+
+
+class VerifyEmail(generics.GenericAPIView):
+    def get(self):
+        pass
