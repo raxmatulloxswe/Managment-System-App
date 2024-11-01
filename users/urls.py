3090657a4334e56421294bfcb48e3d2c400e61f8
Rahmatjon <rahmatjonibragimov17@gmail.com>
2024-11-01 16:38:13 +0500

    added verify-email/ urls

--- users/urls.py
+++ users/urls.py
@@ -1,1 +1,1 @@
-from django.urls import pathfrom .views import RegisterViewurlpatterns = [    path('api/register/', RegisterView.as_view(), name='register'),]
+from django.urls import pathfrom .views import RegisterView, VerifyEmailurlpatterns = [    path('api/register/', RegisterView.as_view(), name='register'),    path('verify-email/', VerifyEmail.as_view(), name='verify-email'),]
